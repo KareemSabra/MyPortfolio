63420e1449238343794b85d3f323f70e9f0f6671
Kareem Sabra <63441894+KareemSabra@users.noreply.github.com>
2023-12-14 00:40:02 +0200

    initial Commit

---
+++ src/content_option.js
@@ -0,0 +1,174 @@
+const logotext = "KS";
+const meta = {
+  title: "Kareem Sabra",
+  description: "I'm Kareem Sabra, a software engineer and developer.",
+};
+
+const introdata = {
+  title: "Hi, I'm Kareem",
+  animated: {
+    first: "Software Engineer",
+    second: "Frontend Developer",
+    third: "Backend Developer",
+    fourth: "System Designer",
+    // fifth: "Malware Developer",
+    // sixth: "Analyst",
+    // add more if you'd like but make sure to update /src/pages/home/index.js Line 29
+  },
+  description:
+    "Dedicated technology enthusiast with an insatiable curiosity for unraveling the complexities of our digital world.",
+  your_img_url: "./src/assets/images/Avatar.png",
+};
+
+const dataabout = {
+  title: "Who am I?",
+  aboutme:
+    "As an emerging software engineer, I bring over three years of dedicated experience in full stack development, navigating diverse frameworks including ReactJS, NodeJS, Flask, and an array of libraries. My relentless drive centers on embracing new technologies, leveraging a rapid learning curve to fuel my passion for growth. I'm committed to diligent efforts, consistently pursuing and conquering challenges to accomplish my objectives.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Full Stack Developer",
+    where: "Venture Appital",
+    date: "2021 - Present",
+  },
+  {
+    jobtitle: "FreeLance Web Developer",
+    date: "2020",
+  },
+];
+
+const skills = [
+  {
+    name: "Python",
+  },
+
+  {
+    name: "C",
+  },
+
+  {
+    name: "Java",
+  },
+
+  {
+    name: "JavaScript",
+  },
+
+  {
+    name: "NodeJS",
+  },
+
+  {
+    name: "ReactJS",
+  },
+
+  {
+    name: "Flask",
+  },
+
+  {
+    name: "MySQL",
+  },
+];
+
+// const dataportfolio = [
+//   {
+//     img: "https://picsum.photos/400/?grayscale",
+//     description:
+//       "The wisdom of life consists in the elimination of non-essentials.",
+//     link: "#",
+//   },
+//   {
+//     img: "https://picsum.photos/400/800/?grayscale",
+//     description:
+//       "The wisdom of life consists in the elimination of non-essentials.",
+//     link: "#",
+//   },
+//   {
+//     img: "https://picsum.photos/400/?grayscale",
+//     description:
+//       "The wisdom of life consists in the elimination of non-essentials.",
+//     link: "#",
+//   },
+//   {
+//     img: "https://picsum.photos/400/600/?grayscale",
+//     description:
+//       "The wisdom of life consists in the elimination of non-essentials.",
+//     link: "#",
+//   },
+//   {
+//     img: "https://picsum.photos/400/300/?grayscale",
+//     description:
+//       "The wisdom of life consists in the elimination of non-essentials.",
+//     link: "#",
+//   },
+//   {
+//     img: "https://picsum.photos/400/700/?grayscale",
+//     description:
+//       "The wisdom of life consists in the elimination of non-essentials.",
+//     link: "#",
+//   },
+
+//   {
+//     img: "https://picsum.photos/400/600/?grayscale",
+//     description:
+//       "The wisdom of life consists in the elimination of non-essentials.",
+//     link: "#",
+//   },
+//   {
+//     img: "https://picsum.photos/400/300/?grayscale",
+//     description:
+//       "The wisdom of life consists in the elimination of non-essentials.",
+//     link: "#",
+//   },
+//   {
+//     img: "https://picsum.photos/400/?grayscale",
+//     description:
+//       "The wisdom of life consists in the elimination of non-essentials.",
+//     link: "#",
+//   },
+//   {
+//     img: "https://picsum.photos/400/550/?grayscale",
+//     description:
+//       "The wisdom of life consists in the elimination of non-essentials.",
+//     link: "#",
+//   },
+//   {
+//     img: "https://picsum.photos/400/?grayscale",
+//     description:
+//       "The wisdom of life consists in the elimination of non-essentials.",
+//     link: "#",
+//   },
+//   {
+//     img: "https://picsum.photos/400/700/?grayscale",
+//     description:
+//       "The wisdom of life consists in the elimination of non-essentials.",
+//     link: "#",
+//   },
+// ];
+
+const contactConfig = {
+  YOUR_EMAIL: "test@test.com",
+  description: "Looking forward to hearing from you!",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "SERVICE_ID",
+  YOUR_TEMPLATE_ID: "TEMPLATE_ID",
+  YOUR_USER_ID: "USER_ID (PUBLIC_KEY)",
+};
+
+const socialprofils = {
+  github: "https://github.com/KareemSabra",
+  linkedin: "https://www.linkedin.com/in/kareem20sabra/",
+};
+export {
+  meta,
+  dataabout,
+//   dataportfolio,
+  worktimeline,
+  skills,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
